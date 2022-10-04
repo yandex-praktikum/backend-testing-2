315c835fdfaae6dbbd8351e47c4e2e70c5b7a6cd
zlocate <zlocated@gmail.com>
2022-10-04 18:32:27 +0300

    feat: add solution

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -9,10 +9,10 @@ describe('PostsService', () => {
 
   describe('.findMany', () => {
     const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
+      { text: 'Post 1' },
+      { text: 'Post 2' },
+      { text: 'Post 3' },
+      { text: 'Post 4' },
     ];
 
     beforeEach(() => {
@@ -20,13 +20,48 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const foundPosts = postsService.findMany();
+
+      expect(foundPosts).toHaveLength(posts.length);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(posts.map(expect.objectContaining)),
+      );
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const limit = 2;
+      const skip = 1;
+
+      const foundPosts = postsService.findMany({ skip, limit });
+
+      expect(foundPosts).toHaveLength(limit);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(
+          posts.slice(skip, limit).map(expect.objectContaining),
+        ),
+      );
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('should return correct posts for skip option', () => {
+      const skip = 2;
+      const foundPosts = postsService.findMany({ skip });
+
+      expect(foundPosts).toHaveLength(posts.length - skip);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(posts.slice(skip).map(expect.objectContaining)),
+      );
+    });
+
+    it('should return correct posts for limit option', () => {
+      const limit = 3;
+      const foundPosts = postsService.findMany({ limit });
+
+      expect(foundPosts).toHaveLength(limit);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(
+          posts.slice(0, limit).map(expect.objectContaining),
+        ),
+      );
+    });
   });
 });
