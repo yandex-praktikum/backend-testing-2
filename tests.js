6a949142f3df83e83b0b42fa95e033d0ee06904f
Ilya Vasilyev <vasilyev.i123@gmail.com>
2023-03-14 13:49:23 +0300

    refactor: add error output when the stryker falls

--- tests.js
+++ tests.js
@@ -4,31 +4,35 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
 async function test() {
-    const errors = [];
+  const errors = [];
 
-    const originalConfig = `{"$schema":"./node_modules/@stryker-mutator/core/schema/stryker-schema.json","_comment":"Thisconfigwasgeneratedusing'strykerinit'.Pleasetakealookat:https://stryker-mutator.io/docs/stryker-js/configuration/formoreinformation","packageManager":"npm","plugins":["@stryker-mutator/jest-runner"],"reporters":["json"],"testRunner":"jest","jest":{"projectType":"custom","configFile":"./jest.config.json"},"mutate":["./src/posts.service.ts:33-47"],"thresholds":{"break":100},"concurrency":2}`;
-    const config = readFileSync(join(__dirname, 'stryker.conf.json'), 'utf-8').replace(/[\n\r]/g, '').replace(/\s/g, '');
-    if (config !== originalConfig) {
-        throw new Error(`Не исправляйте и не удаляйте файл stryker.conf.json`)
-    }
+  const originalConfig = `{"$schema":"./node_modules/@stryker-mutator/core/schema/stryker-schema.json","_comment":"Thisconfigwasgeneratedusing'strykerinit'.Pleasetakealookat:https://stryker-mutator.io/docs/stryker-js/configuration/formoreinformation","packageManager":"npm","plugins":["@stryker-mutator/jest-runner"],"reporters":["json"],"testRunner":"jest","jest":{"projectType":"custom","configFile":"./jest.config.json"},"mutate":["./src/posts.service.ts:33-47"],"thresholds":{"break":100},"concurrency":2}`;
+  const config = readFileSync(join(__dirname, 'stryker.conf.json'), 'utf-8').replace(/[\n\r]/g, '').replace(/\s/g, '');
+  if (config !== originalConfig) {
+    throw new Error(`Не исправляйте и не удаляйте файл stryker.conf.json`)
+  }
 
-    try {
-        await exec('stryker run');
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    await exec('stryker run');
+  } catch(error) {
+    let message = `Тесты завершились с ошибкой: ${error.message}`;
+    message += error.stdout ? `\nЛоги тестов: ${error.stdout}` : '';
+    message += error.stderr ? `\nКритические ошибки: ${error.stderr}` : '';
+    console.log(message);
+    process.exit(1);
+  }
 
-    const mutationsErrors = JSON.parse(readFileSync(join(__dirname, 'mutations-errors.json'), 'utf-8'));
-    const mutation = JSON.parse(readFileSync(join(__dirname, 'reports', 'mutation', 'mutation.json'), 'utf-8'));
-    const error = mutation.files['src/posts.service.ts'].mutants.find(({ status }) => status === 'Survived');
+  const mutationsErrors = JSON.parse(readFileSync(join(__dirname, 'mutations-errors.json'), 'utf-8'));
+  const mutation = JSON.parse(readFileSync(join(__dirname, 'reports', 'mutation', 'mutation.json'), 'utf-8'));
+  const error = mutation.files['src/posts.service.ts'].mutants.find(({ status }) => status === 'Survived');
 
-    if (error) {
-        const { error: messageError } = mutationsErrors.find(({ id }) => id === error.id);
-        errors.push(messageError);
-        throw new Error(errors.join('\n'))
-    } else {
-        console.log("Тесты пройдены")
-    }
+  if (error) {
+    const { error: messageError } = mutationsErrors.find(({ id }) => id === error.id);
+    errors.push(messageError);
+    throw new Error(errors.join('\n'));
+  } else {
+    console.log("Тесты пройдены");
+  }
 }
 
 test();
